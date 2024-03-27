d7ec585a8912e81110cdaff7876aef501fd8adb0
Sebastian Vikstrom <>
2024-03-27 11:13:33 +0100

    Update page content and add @tailwindcss/typography plugin

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,21 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center">
       <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20 py-24">
-        <div className="mb-5">
-          <h1 className="text-3xl mb-2">Enkelspårigt</h1>
-          <p className="max-w-prose">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit eius
-            odio voluptate natus, doloremque quisquam deserunt et, fugit omnis,
-            autem beatae ipsa! Quis beatae, exercitationem eius optio alias ex
-            fuga.
+        <div className="prose lg:prose-xl mb-4">
+          <p>
+            Alla järnvägar i norra Norrland är enkelspåriga. Den gäller även den
+            hårt belastade Malmbanan mellan Luleå och Narvik där gruvbolagen
+            transporterar sina produkter till hamnar och stålverk samt Stambanan
+            som förbinder norra och södra Sverige med gods- och persontrafik.
+            Det handlar om hundratals mil av samhällsviktiga transportleder som
+            saknar dubbelspår för mötande trafik.
+          </p>
+          <p>
+            Enkelspåren gör dem till flaskhalsar och extra känsliga för
+            störningar. En olycka med trafikstopp på Malmbanan kostar
+            gruvbolagen 100 miljoner per dag i förlorade inkomster, vilket i
+            förlängningen innebär miljarder i minskade intäkter till den svenska
+            statskassan och välfärden. Enkelspårigt agerande från politiken.
           </p>
         </div>
         <NewsCards />
